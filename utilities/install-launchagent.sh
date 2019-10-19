34139d04949528a56c035a1831df606ccbef96b6
Dan Alvidrez <dan@bayareawebpro.com>
2019-10-19 10:00:49 -0700

    Update install-launchagent.sh

--- utilities/install-launchagent.sh
+++ utilities/install-launchagent.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Usage: install-launchagent.sh ./com.digitalocean.backup.xml
+# launchctl unload -w ~/Library/LaunchAgents/com.a1auto-digitalocean.backup.plist
+# $(which sh) ~/BackupTool/utilities/install-launchagent.sh ~/BackupTool/scripts/com.a1auto-digitalocean.backup.xml
 
 # Agent Source File
 if [[ $# -eq 0 && -f $1 ]]; then
@@ -17,36 +18,38 @@ AGENT=~/Library/LaunchAgents/${AGENT_NAME}.plist
 
 # Get Agent Installation Status
 function agentIsInstalled(){
-    launchctl list | grep -q ${AGENT_NAME}
+    launchctl list | grep -q "${AGENT_NAME}"
 }
 
 # Remove Existing Agent.
 if agentIsInstalled; then
     echo "Uninstalling Existing Agent..."
-    launchctl unload -w ${AGENT}
+    launchctl unload -w "${AGENT}"
 fi
 
 # Copy New Agent.
 echo "Copying New Agent: $SOURCE_FILE..."
-cp ${SOURCE_FILE} ${AGENT}
+rm "${AGENT}"
+cp "${SOURCE_FILE}" "${AGENT}"
 
 # Read PropertyList for Associated Script
-SCRIPT=$(defaults read ${AGENT} Program)
+SCRIPT=$(defaults read "${AGENT}" Program)
 
 # Set Script Executable Permissions.
 if [[ -f ${SCRIPT} ]]; then
-    chmod a+x ${SCRIPT};
+    chmod a+x "${SCRIPT}";
     echo "Executable Permissions Set: $SCRIPT..."
 fi
 
 # Install New Agent.
 echo "Installing New Agent: $AGENT..."
-launchctl load -w ${AGENT}
+launchctl load -w "${AGENT}"
 
 # Verify New Agent is Installed.
 if agentIsInstalled; then
-    echo "Agent Installed Successfully: Running $AGENT_NAME..."
+    echo "Agent Installed Successfully."
     #launchctl start ${AGENT}
+    #echo "Running Agent..."
 else
     echo "Agent Installation Failed."
 fi
